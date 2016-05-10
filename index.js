040d4f5342a1c8024329948a17fceec0a1e902d7
juan.sanchez <juan.sanchez@viapool.com>
2016-05-10 18:01:37 -0300

    change config source in index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-var fs = require('fs');
-var config = JSON.parse( fs.readFileSync("./.ealertapirc") );
+var config = require('./lib/config-loader.js');
 var app = require('./lib/ealert-api.js')(config);
